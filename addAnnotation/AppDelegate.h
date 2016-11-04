a0c542410ff4cd1dc4d8f30ed462fe2f1320c1e9
DineshKumar Toofran <dineshkumartoofran@DineshKumars-MacBook-Pro.local>
2016-11-04 11:11:24 +0530

    Initial Commit

---
+++ addAnnotation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  addAnnotation
+//
+//  Created by DineshKumar Toofran on 04/11/16.
+//  Copyright © 2016 DineshKumar Toofran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ addAnnotation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  addAnnotation
+//
+//  Created by DineshKumar Toofran on 04/11/16.
+//  Copyright © 2016 DineshKumar Toofran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
